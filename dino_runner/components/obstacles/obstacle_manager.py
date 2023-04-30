76e4baac08954253d1014e538a98ad2d232ad08f
AlissonOspina <Alisson.Ospina@fundacion-jala.org>
2023-04-30 02:03:49 -0500

    Second-Class-Homework

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,20 +1,28 @@
 import pygame
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
-    def generate_obstacle(self):
-       obstacle = Cactus(SMALL_CACTUS)
-
-       return obstacle
+    def generate_obstacle(self, oType):
+        if oType == 0:
+           obstacle = Cactus(SMALL_CACTUS)            #Small_cactus
+        elif oType == 1:
+           obstacle = Cactus(LARGE_CACTUS)            #Large_cactus
+        elif oType == 2:
+           obstacle = Bird(BIRD)                      #ave
+        return obstacle    
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            obstacle = self.generate_obstacle()
+            oType = random.randint(0,2)
+            obstacle = self.generate_obstacle(oType)
             self.obstacles.append(obstacle)
 
         for obstacle in self.obstacles:
